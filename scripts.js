c707bf517fb999d1a6a7b0160d4dbea727adb9c8
Steven <stevenaknobel@gmail.com>
2024-10-24 21:18:54 +0200

    create a try and block and error handling for using either 0 or NaN

--- scripts.js
+++ scripts.js
@@ -6,4 +6,30 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
   result.innerText = dividend / divider;
+//create a try block to catch the errors and force the inputs
+  try {
+    //! checks if the values for dividend and divider are falsy (checks if either of these is empty)
+    if (!dividend || !divider) {
+      result.innerText = "Division not performed. Both values are required in inputs. Try again.";
+      return;
+    }
+
+    //convert the string inputs into numbers
+    const dividendNumber = parseFloat(dividend);
+    const dividerNumber = parseFloat(divider);
+
+    //check whether or not the inputs are numbers or not using NaN
+    if (isNaN(dividendNumber) || isNaN(dividerNumber)) {
+      throw new Error("Invalid input: non-numeric value.")
+    }
+
+    //check if they try and divide by 0
+    if (dividerNumber === 0) {
+      result.innerText = "Division not performed. Invalid number provided. Try again."
+      //create a new error and error stack for debuggging
+      console.error(new Error("Division by Zero error"), new Error().stack);
+      return;
+    }
+  }
 });
+
