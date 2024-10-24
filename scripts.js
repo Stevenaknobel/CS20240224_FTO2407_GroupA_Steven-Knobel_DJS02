a928d97ddb2806e5df142d0a0fc4fd99768d0871
Steven <stevenaknobel@gmail.com>
2024-10-24 22:07:07 +0200

    checked and added references for each user story from the readme

--- scripts.js
+++ scripts.js
@@ -9,6 +9,7 @@ form.addEventListener("submit", (event) => {
 //create a try block to catch the errors and force the inputs
   try {
     //! checks if the values for dividend and divider are falsy (checks if either of these is empty)
+    //user story 2 ensuring both fields are filled in using a falsy check
     if (!dividend || !divider) {
       result.innerText = "Division not performed. Both values are required in inputs. Try again.";
       return;
@@ -19,11 +20,13 @@ form.addEventListener("submit", (event) => {
     const dividerNumber = parseFloat(divider);
 
     //check whether or not the inputs are numbers or not using NaN
+    //user story 4 check that valid numbers are used with NaN (Not a Number)
     if (isNaN(dividendNumber) || isNaN(dividerNumber)) {
       throw new Error("Invalid input: non-numeric value.")
     }
 
     //check if they try and divide by 0
+    //user story 3 check if 0 is used and if it is pull and error
     if (dividerNumber === 0) {
       result.innerText = "Division not performed. Invalid number provided. Try again."
       //create a new error and error stack for debuggging
@@ -32,8 +35,10 @@ form.addEventListener("submit", (event) => {
     }
 
     //perform the division calculation resulting in a whole number
+    //user story no.1 using math floor to ensure whole numbers
     const wholeNumberResult = Math.floor(dividendNumber / dividerNumber);
     result.innerText = wholeNumberResult;
+
   //add a catch to handle errors
   } catch (error) {
     document.body.innerHTML = `<div class="critical error">Something critical went wrong. Please reload the page.</div>`;
